49c05ba06114c82c1e00ff44e4a80dadd433a001
Maria Jose Gonzalez Guzman <mguzman@bapcorporativo.com>
2023-07-25 16:58:54 -0700

    PATCH method added

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*') // o el dominio específico que esté haciendo la solicitud
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next(); // Llama a next() para continuar con el procesamiento de las rutas
   })
