b07bd6ffd6c6b40971bba47c8af212b18c8f1a5d
Maria Jose Gonzalez Guzman <mguzman@bapcorporativo.com>
2023-07-25 16:41:26 -0700

    fix access control

--- src/app.js
+++ src/app.js
@@ -18,6 +18,13 @@ const app =express()
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*') // o el dominio específico que esté haciendo la solicitud
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next(); // Llama a next() para continuar con el procesamiento de las rutas
+  })
+
 app.use('/api',studentInfoRoutes)
 app.use('/api',careersRoutes)
 app.use('/api',gendersRoutes)
@@ -27,12 +34,6 @@ app.use('/api',internalAdviserInfoRoutes)
 app.use('/api',companyInfoRoutes)
 app.use(ping)
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*') // o el dominio específico que esté haciendo la solicitud
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    next(); // Llama a next() para continuar con el procesamiento de las rutas
-  })
   app.use((req, res, next) => {
     res.status(404).json({
       message: 'Endpoint not found'
