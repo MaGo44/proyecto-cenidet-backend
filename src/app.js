dd7a304622f88743905d0b9e6c8804925bcef467
Maria Jose Gonzalez Guzman <mguzman@bapcorporativo.com>
2023-07-25 16:23:36 -0700

    access control added

--- src/app.js
+++ src/app.js
@@ -27,10 +27,16 @@ app.use('/api',internalAdviserInfoRoutes)
 app.use('/api',companyInfoRoutes)
 app.use(ping)
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*') // o el dominio específico que esté haciendo la solicitud
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next(); // Llama a next() para continuar con el procesamiento de las rutas
+  })
+  app.use((req, res, next) => {
     res.status(404).json({
-        message:'Endpoint not found'
+      message: 'Endpoint not found'
     })
-})
+  })
 
 export default app;
