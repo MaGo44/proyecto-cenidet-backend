9fb981b3f2e03dc2a392e1bb33704d170252cc32
Maria Jose Gonzalez Guzman <mguzman@bapcorporativo.com>
2023-08-07 02:11:37 -0700

    fix

--- src/controllers/student_documents.controller.js
+++ src/controllers/student_documents.controller.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
       cb(null, 'uploadDocuments')
   },
   filename: function (req, file, cb) {
-      cb(null, `${Date.now()}-${file.originalname}`)
+    cb(null, file.originalname);
   }
 })
 
